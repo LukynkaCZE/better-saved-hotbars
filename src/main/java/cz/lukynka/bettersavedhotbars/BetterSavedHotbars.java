9f5742ab50b486b6ef02d4efdaa941e36b46372f
LukynkaCZE <jiraklukas058@seznam.cz>
2024-03-25 21:06:14 +0100

    Add updater
    Updater by AsoDesu_ from IslandUtils

--- src/main/java/cz/lukynka/bettersavedhotbars/BetterSavedHotbars.java
+++ src/main/java/cz/lukynka/bettersavedhotbars/BetterSavedHotbars.java
@@ -1,19 +1,41 @@
 package cz.lukynka.bettersavedhotbars;
 
+import cz.lukynka.bettersavedhotbars.updater.UpdateManager;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Optional;
+
 public class BetterSavedHotbars implements ModInitializer {
-    // This logger is used to write text to the console and the log file.
-    // It is considered best practice to use your mod id as the logger's name.
-    // That way, it's clear which mod wrote info, warnings, and errors.
+
     public static final Logger LOGGER = LoggerFactory.getLogger("bettersavedhotbars");
+
+    public static String version = "";
+
+    public static String updateTag = "";
+
+    public static boolean isDevVersion = false;
+
+    public static UpdateManager updateManager = new UpdateManager();
+
+    public static boolean updateAvailable = false;
 
     public static float lastScrollOffset = 0;
 
     @Override
     public void onInitialize() {
         LOGGER.info("Hi there");
+
+        Optional<ModContainer> container = FabricLoader.getInstance().getModContainer("bettersavedhotbars");
+        container.ifPresent(modContainer -> version = modContainer.getMetadata().getVersion().getFriendlyString());
+
+        try {
+            updateManager.runUpdateCheck();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/cz/lukynka/bettersavedhotbars/updater/AvailableUpdate.java
@@ -0,0 +1,3 @@
+package cz.lukynka.bettersavedhotbars.updater;
+
+public record AvailableUpdate(String title, String version, String releaseUrl) {}
